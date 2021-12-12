ca21cf8b296db2bd4a733d12a6e90313c365b084
Afifa-web <afifauttara444@gmail.com>
2021-12-13 04:49:13 +0600

    first commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,7 +10,7 @@ class ContactController extends Controller
 {
     public function contact()
     {
-      return view('frontend.layouts.contact-me');
+      return view('frontend.index');
     }
 
     public function sendEmail(Request $request)
