4a4efed562698911b152d9aadcf8c5a8a0dc1a0f
Afifa-web <afifauttara444@gmail.com>
2021-12-13 05:36:10 +0600

    first commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -14,18 +14,18 @@
   <meta name="keywords" content="">
   <meta name="author" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  {{-- <link rel="stylesheet" href=" http://laravprotfolio.herokuapp.com/frontend/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href=" http://laravprotfolio.herokuapp.com/frontend/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href=" http://laravprotfolio.herokuapp.com/frontend/assets/css/magnific-popup.css">
     <link rel="stylesheet" href=" http://laravprotfolio.herokuapp.com/frontend/assets/css/font-awesome.min.css">
 
     <!-- MAIN CSS -->
-    <link rel="stylesheet" href="http://laravprotfolio.herokuapp.com/frontend/assets/css/templatemo-style.css"> --}}
-  <link rel="stylesheet" href=" {{ asset('frontend/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="http://laravprotfolio.herokuapp.com/frontend/assets/css/templatemo-style.css">
+  {{-- <link rel="stylesheet" href=" {{ asset('frontend/assets/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href=" {{ asset('frontend/assets/css/magnific-popup.css') }}">
   <link rel="stylesheet" href=" {{ asset('frontend/assets/css/font-awesome.min.css') }}">
 
   <!-- MAIN CSS -->
-  <link rel="stylesheet" href="{{ asset('frontend/assets/css/templatemo-style.css') }}">
+  <link rel="stylesheet" href="{{ asset('frontend/assets/css/templatemo-style.css') }}"> --}}
 
 </head>
 <body>
@@ -709,11 +709,17 @@
        </div>
   </section>
 </body>
-  <script src="{{ asset('frontend/assets/js/jquery.js') }}"></script>
+<script src="http://laravprotfolio.herokuapp.com/frontend/assets/js/jquery.js"></script>
+<script src="http://laravprotfolio.herokuapp.com/frontend/assets/js/bootstrap.min.js"></script>
+<script src="http://laravprotfolio.herokuapp.com/frontend/assets/js/jquery.stellar.min.js"></script>
+<script src="http://laravprotfolio.herokuapp.com/frontend/assets/js/jquery.magnific-popup.min.js"></script>
+<script src="http://laravprotfolio.herokuapp.com/frontend/assets/js/smoothscroll.js"></script>
+<script src="http://laravprotfolio.herokuapp.com/frontend/assets/js/custom.js"></script>
+  {{-- <script src="{{ asset('frontend/assets/js/jquery.js') }}"></script>
   <script src="{{ asset('frontend/assets/js/bootstrap.min.js') }}"></script>
   <script src="{{ asset('frontend/assets/js/jquery.stellar.min.js') }}"></script>
   <script src="{{ asset('frontend/assets/js/jquery.magnific-popup.min.js') }}"></script>
   <script src="{{ asset('frontend/assets/js/smoothscroll.js') }}"></script>
-  <script src="{{ asset('frontend/assets/js/custom.js') }}"></script>
+  <script src="{{ asset('frontend/assets/js/custom.js') }}"></script> --}}
   </html>
 {{-- @endsection --}}
