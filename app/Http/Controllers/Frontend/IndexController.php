34d5c71772b25bf82eb09a8b354ba79ce2deef81
Afifa-web <afifauttara444@gmail.com>
2021-12-13 05:09:48 +0600

    first commit

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Frontend;
-
+use App\Mail\ContactMail;
+use Mail;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,21 @@ class IndexController extends Controller
   public function home()
   {
     return view('frontend.index');
-}
+  }
+  public function contact()
+  {
+    return view('frontend.index');
+  }
+  public function sendEmail(Request $request)
+  {
+    $details = [
+      'name'=>$request->name,
+      'email'=>$request->email,
+      'phone'=>$request->phone,
+      'message'=>$request->message,
+    ];
+    Mail::to('afifauttara444@gmail.com')->send(new ContactMail($details));
+    return back()->with('message_sent','Your message has been sent successfully!');
+  }
 
   }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 //front end start
 Route::get('/', [App\Http\Controllers\Frontend\IndexController::class, 'home'])->name('home');
-Route::get('/', [ContactController::class, 'contact']);
-Route::Post('/send-message',[ContactController::class, 'sendEmail'])->name('contact.send');
+Route::get('/', [App\Http\Controllers\Frontend\IndexController::class, 'contact'])-;
+// Route::get('/', [ContactController::class, 'contact']);
+Route::Post('/send-message',[IndexController::class, 'sendEmail'])->name('contact.send');
 //front end end
