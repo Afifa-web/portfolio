33e80d2b2b87e054b02fcdb5b543fc1ef61e451e
Afifa-web <afifauttara444@gmail.com>
2021-12-16 22:16:41 +0600

    first commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\ContactMail;
+// use App\Mail\PHPMailer;
 use Illuminate\Http\Request;
-use Mail;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+// use Mail;
 
 class ContactController extends Controller
 {
@@ -15,13 +18,38 @@ class ContactController extends Controller
 
     public function sendEmail(Request $request)
     {
-      $details = [
-        'name'=>$request->name,
-        'email'=>$request->email,
-        'phone'=>$request->phone,
-        'message'=>$request->message,
-      ];
-      Mail::to('afifauttara444@gmail.com')->send(new ContactMail($details));
-      return back()->with('message_sent','Your message has been sent successfully!');
+      // $details = [
+        $name=>$request->name,
+        $email=>$request->email,
+        $phone=>$request->phone,
+        $message=>$request->message,
+
+        // require 'PHPMailer/vendor/autoload.php';
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+        $mail = new PHPMailer(true);
+        $mail ->SMTPDebug =0;
+        $mail ->isSMTP();
+        $mail ->env('smtp.mailtrap.io');
+        $mail ->SMTPAuth=true;
+        $mail ->Username =env('MAIL_USERNAME');
+        $mail ->Password =env('MAIL_PASSWORD');
+        $mail ->SMTPSecure =PHPMailer::ENCRYPTION_STARTTLS;
+        $mail-> Port =2525;
+        $mail ->setFrom($name,$email);
+        $mail ->addAddress('afifauttara444@gmail.com');
+        $mail ->Subject='$phone';
+        $mail ->Body='$message';
+        $dt=$mail->send();
+      if($dt){
+          return back()->with('message_sent','Your message has been sent successfully!');
+      }else {
+        echo "Something wet wrong";
+      }
+      // ];
+      // Mail::to('afifauttara444@gmail.com')->send(new PHPMailer($details));
+      // return back()->with('message_sent','Your message has been sent successfully!');
     }
 }
