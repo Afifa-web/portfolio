78e861b73f71b17120ad305620a08dbab02dcdf3
Afifa-web <afifauttara444@gmail.com>
2021-12-16 23:23:27 +0600

    first commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-// use App\Mail\PHPMailer;
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-// use Mail;
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
+use Mail;
 
 class ContactController extends Controller
 {
@@ -18,38 +18,43 @@ class ContactController extends Controller
 
     public function sendEmail(Request $request)
     {
-      // $details = [
-        $name=$request->name;
-        $email=$request->email;
-        $phone=$request->phone;
-        $message=$request->message;
+      $details = [
+        // $name=$request->name;
+        // $email=$request->email;
+        // $phone=$request->phone;
+        // $message=$request->message;
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'phone'=>$request->phone,
+        'message'=>$request->message,
 
         // require 'PHPMailer/vendor/autoload.php';
-
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-        $mail = new PHPMailer(true);
-        $mail ->SMTPDebug =0;
-        $mail ->isSMTP();
-        $mail ->Host =env('MAIL_HOST');
-        $mail ->SMTPAuth=true;
-        $mail ->Username =env('MAIL_USERNAME');
-        $mail ->Password =env('MAIL_PASSWORD');
-        $mail ->SMTPSecure =PHPMailer::ENCRYPTION_STARTTLS;
-        $mail-> Port =2525;
-        $mail ->setFrom($name,$email);
-        $mail ->addAddress('afifauttara444@gmail.com');
-        $mail ->Subject='$phone';
-        $mail ->Body='$message';
-        $dt=$mail->send();
-      if($dt){
-          return back()->with('message_sent','Your message has been sent successfully!');
-      }else {
-        echo "Something wet wrong";
-      }
-      // ];
-      // Mail::to('afifauttara444@gmail.com')->send(new PHPMailer($details));
-      // return back()->with('message_sent','Your message has been sent successfully!');
+//
+// require 'PHPMailer/src/Exception.php';
+// require 'PHPMailer/src/PHPMailer.php';
+// require 'PHPMailer/src/SMTP.php';
+//         $mail = new PHPMailer(true);
+//         $mail ->SMTPDebug =0;
+//         $mail ->isSMTP();
+//         $mail ->Host =env('MAIL_HOST');
+//         $mail ->SMTPAuth=true;
+//         $mail ->Username =env('MAIL_USERNAME');
+//         $mail ->Password =env('MAIL_PASSWORD');
+//         $mail ->SMTPSecure =PHPMailer::ENCRYPTION_STARTTLS;
+//         $mail-> Port =587;
+//         $mail ->setFrom($name,$email);
+//         $mail ->addAddress('afifauttara444@gmail.com');
+//         $mail ->isHTML(true);
+//         $mail ->Subject='$phone';
+//         $mail ->Body='$message';
+//         $dt=$mail->send();
+//       if($dt){
+//           return back()->with('message_sent','Your message has been sent successfully!');
+//       }else {
+//         echo "Something wet wrong";
+//       }
+      ];
+      Mail::to('afifauttara444@gmail.com')->send(new ContactMail($details));
+      return back()->with('message_sent','Your message has been sent successfully!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MailtestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 //front end start
+
 Route::get('/', [App\Http\Controllers\Frontend\IndexController::class, 'home'])->name('home');
 // Route::get('/', [App\Http\Controllers\Frontend\IndexController::class, 'contact']);
 Route::get('contact', [ContactController::class, 'contact']);
 Route::Post('/send-message',[ContactController::class, 'sendEmail'])->name('contact.send');
 //front end end
+Route::get('/testmail', [MailtestController::class, 'index']);
+// Route::middleware(['auth:sanctum','verified'])->get('contact.send');
